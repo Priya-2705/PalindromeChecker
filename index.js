1cdd93a62e2393dab1c9b10cc3c4db880bfe6978
PRIYA <priyapillarisetty19@gmail.com>
2025-03-24 17:27:20 -0400

    Replaced alert with on screen display

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const input = document.getElementById("input");
+
 
 function cleanString(str) {
     return str.toLowerCase().replace(/[^a-z0-9]/g, "");
@@ -9,16 +9,24 @@ function reverseString(str) {
 }
 
 function check() {
-    const value = input.value;
-    const cleanedValue = cleanString(value);
+    const input = document.getElementById("input").value;
+    const result = document.getElementById("result");
+    const cleanedValue = cleanString(input);
     const reversed = reverseString(cleanedValue);
     
-    if(cleanedValue === reversed) {
-        alert("P A L I N D R O M E");
+    if(cleanedValue == "") {
+        result.textContent = "Please enter a word"
+        result.style.color = "red";
+    }
+    else if(cleanedValue === reversed) {
+        result.textContent = `"${input} is a P A L I N D R O M E"`;
+        result.style.color = "green"
     }
     else {
-        alert("N O T   A   P A L I N D R O M E");
+        result.textContent = `"${input} is N O T     A     P A L I N D R O M E"`;
+        result.style.color = "red";
     }
+    result.style.display = "block";
 
-    input.value = "";
+    document.getElementById("input").value = "";
 }
